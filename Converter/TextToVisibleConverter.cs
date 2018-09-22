7521655fd8fa613bba813db409c8fb9351dc0f55
deadkitty <stauch.basti@hotmail.de>
2018-09-22 19:45:52 +0200

    TextToVisible Converter

---
+++ Converter/TextToVisibleConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Nyantilities.Converter
+{
+    public class TextToVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            String text = value as String;
+
+            return text != null && text != String.Empty ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
